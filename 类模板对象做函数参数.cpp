2f17764a0766f38517cded2c70d4d391679e7888
xiangjuan <647668693@qq.com>
2026-01-29 19:48:46 +0800

    添加了类模板做函数参数代码

---
+++ 类模板对象做函数参数.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string>
+using namespace std;
+template<class NameType,class AgeType>
+class Person
+{
+public:
+	Person(NameType name,AgeType age)
+	{
+		m_Name=name;
+		m_Age=age;
+	}
+	void showPerson()
+	{
+		cout<<"name: "<<m_Name<<" age ="<<m_Age<<endl;
+	}
+	NameType m_Name;
+	AgeType m_Age;
+};
+//1.指定传入类型
+void printPerson1(Person<string,int> &p)
+{
+	p.showPerson();
+}
+void test01()
+{
+	Person<string,int>p("孙悟空",100);
+	printPerson1(p);
+}
+//2.参数模板化
+template<class T1,class T2>
+void printPerson2(Person<T1, T2>&p)
+{
+	p.showPerson();
+	cout<<"T1的类型为："<<typeid(T1).name() <<endl;
+	cout<<"T2的类型为："<<typeid(T2).name() <<endl;
+}
+void test02()
+{
+	Person<string, int>p("猪八戒",90);
+	printPerson2(p);
+}
+//3.整个类模板化
+template<class T>
+void printPerson3(T &p)
+{
+    p.showPerson();
+}
+void test03()
+{
+    Person<string,int>p("唐僧",30);
+    printPerson3(p);
+}
+int main()
+{
+	test01();
+	test02();
+    test03();
+	return 0;
+}
