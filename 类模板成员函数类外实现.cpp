be1e27f64de0aa5c9d8c307fb80b566bfdc754d0
xiangjuan <647668693@qq.com>
2026-01-30 11:42:26 +0800

    添加了类模板类外实现代码

---
+++ 类模板成员函数类外实现.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+template<class T1, class T2>
+class Person{
+public:
+	Person(T1 name,T2 age);
+	//成员函数类内声明
+	void showPerson();
+public:
+	T1 m_Name;
+	T2 m_Age;
+};
+//构造函数 类外实现
+template<class T1, class T2>
+Person<T1, T2>::Person(T1 name,T2 age)
+{
+	m_Name = name;
+	m_Age = age;
+}
+//成员函数 类外实现
+template<class T1, class T2>
+void Person<T1, T2>::showPerson()
+{
+	cout<< "姓名：" << m_Name << " 年龄：" <<m_Age<<endl;
+}
+void test01()
+{
+	Person<string, int> p("Tom",19);
+	p.showPerson();
+}
+int main()
+{
+	test01();
+	return 0;
+}
